f6a52bfac9c5d1e6ebbf8b204c93e182db8bbfdf
Coffin <sudippaudel512@gmail.com>
2026-05-29 22:52:41 +0545

    create all sql tables

--- backend/app/utils/createtable.py
+++ backend/app/utils/createtable.py
@@ -1,7 +1,11 @@
 import asyncio
-from app.utils.database import engine
-from app.models.usersmodel import Base
+from app.utils.database import engine,Base
+from app.utils.database import Base
 
+from app.models.usersmodel import User, AdminActions
+from app.models.itemmodels import Item
+from app.models.claimmodels import Claim
+from app.models.categorymodels import Category
 
 async def create_tables():
     async with engine.begin() as conn:
