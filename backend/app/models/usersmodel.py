b4eada5af8de00df6e1361a0007ad7ca3132cc9b
Coffin <sudippaudel512@gmail.com>
2026-05-29 22:50:53 +0545

    added adminactions

--- backend/app/models/usersmodel.py
+++ backend/app/models/usersmodel.py
@@ -1,36 +1,54 @@
+from datetime import datetime
+from enum import Enum
 from typing import Optional
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Boolean, String
+from sqlalchemy import Boolean, DateTime, ForeignKey, String, Text
 from pydantic import BaseModel
 
+from app.utils.database import Base
 
-class Base(DeclarativeBase):
-    pass
-
+# enum class for user role
+class Role(Enum):
+    ADMIN = "admin"
+    USER = "user"
 
 class User(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(primary_key=True, index=True)
-    username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    user_id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    fullname: Mapped[str] = mapped_column(String(100), nullable=False)
     email: Mapped[str] = mapped_column(String(100), unique=True)
-    password: Mapped[str] = mapped_column(String(100), nullable=False)
-    role: Mapped[str] = mapped_column(String(100), default="user", nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    role: Mapped[Role] = mapped_column(default=Role.USER, nullable=False)
+    phone: Mapped[str] = mapped_column(String(20))
+    department: Mapped[str] = mapped_column(String(100), nullable=False) 
+    created_at: Mapped[datetime] = mapped_column(DateTime,nullable=False)
+
+# class for keeping tracks of what admin does
+class AdminActions(Base):
+    __tablename__ = "admin_actions"
+    action_id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    admin_id: Mapped[int]  = mapped_column(ForeignKey("users.user_id"),nullable=False)
+    item_id: Mapped[int]  = mapped_column(ForeignKey("items.item_id"),nullable=False)
+    remarks: Mapped[str] = mapped_column(Text, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime,nullable=False)
 
 
+# class for register Form
 class UserCreate(BaseModel):
-    username: str
-    password: str
-    role: str
     email: str
+    password: str
+    full_name: str
+    phone: Optional[str]
+    department: str
+    role: str
 
-
+# class for returning after login
 class UserResponse(BaseModel):
     id: int
-    username: str
+    email: str
+    department: str
     role: str
-    is_active: bool
 
     class config:
         from_attributes = True
@@ -42,9 +60,10 @@ class Token(BaseModel):
 
 
 class TokenData(BaseModel):
-    username: Optional[str] = None
+    email: Optional[str] = None
 
 
+# class for login form
 class UserLogin(BaseModel):
-    username: str
+    email: str
     password: str
